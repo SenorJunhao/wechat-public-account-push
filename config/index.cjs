02d428ee080c7a31a64e5e31e568fedd67f2d438
毛阿毛 <85787982+SenorJunhao@users.noreply.github.com>
2022-11-08 16:14:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,4 +1,4 @@
- const USER_CONFIG = {
+const USER_CONFIG = {
   
     // 请将以下代码按照图示粘贴到对应位置
     USE_PASSAGE: 'server-chan',
@@ -41,27 +41,11 @@
         //   { keyword: 'marry_day', date: '2022-09-09' },
         // ],
       },
-      {
-        // 想要发送的人的名字
-        name: '徐俊豪',
-        // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-        id: 'SCT181218TiljRnMZyLb1ck2G23g2DgR4o',
-        // 使用微信测试号：你想对他发送的模板消息的模板ID
-        useTemplateId: '0002',
-        // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-        horoscopeDate: '04-03',
-        festivals: [
-          // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-          {
-            type: '*生日', name: '徐俊豪', year: '2001', date: '04-03',
-          },
-        ],
-      },
     ],
   
   
     // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: 'YthFIqw_rSqXHRUNE11BQIlDLzeBYtLdBp9X2Nsa8S4',
+    CALLBACK_TEMPLATE_ID: '0002',
   
     CALLBACK_USERS: [
       {
